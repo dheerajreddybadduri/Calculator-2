d46a96ad5cab0ed59e6e20681e59f40e4e5d3f1e
dheerajreddybadduri <115022128+dheerajreddybadduri@users.noreply.github.com>
2022-11-08 22:16:11 -0600

    Update CorrectQuestionPage.xaml.cs

--- Calculator-main/CorrectQuestionPage.xaml.cs
+++ Calculator-main/CorrectQuestionPage.xaml.cs
@@ -1,1 +1,14 @@
+using Calculator.Pages;
+using Calculator.Services;
 
+namespace Calculator;
+
+public partial class CorrectQuestionPage : ContentPage
+{
+	public CorrectQuestionPage()
+	{
+		InitializeComponent();
+	}
+
+
+}
