7249dd78c549e09f200aac7508d4eaeda97abef5
dheerajreddybadduri <115022128+dheerajreddybadduri@users.noreply.github.com>
2022-11-08 22:16:29 -0600

    Update CorrectQuestionPage.xaml.cs

--- Calculator-main/CorrectQuestionPage.xaml.cs
+++ Calculator-main/CorrectQuestionPage.xaml.cs
@@ -9,6 +9,12 @@ public partial class CorrectQuestionPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
+    public async void navigateToNextQuestion(object sender, EventArgs e)
+    {
+        var int_num = StorageService.currNum + 1;
+        StorageService.setCurrentPage(int_num);
+        await SecureStorage.Default.SetAsync("qnum", int_num.ToString());
+        await Navigation.PushAsync(new APITestPage());
+    }
 
 }
